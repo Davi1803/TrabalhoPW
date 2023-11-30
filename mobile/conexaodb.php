383d7c59c5ecd10942bb4e91931d50d8e5552b09
davi1803 <92159621+Davi1803@users.noreply.github.com>
2023-11-30 18:10:18 -0300

    Update conexaodb.php

--- mobile/conexaodb.php
+++ mobile/conexaodb.php
@@ -1,24 +1,26 @@
 <?php
-/*
-//Conexão com banco de dados infinity free
-$servername = "sql303.infinityfree.com"; //endereço do servidor
-$username="if0_35400743";
-$password="46fZd3izCXd";
-$db_name="if0_35400743_Teachhelp";*/
 
-//Conexão com banco de dados infinity free
-$servername = "db4free.net"; //endereço do servidor
-$username="teachhelp";
-$password="d4pnw-d3@*ZRNkF";
-$db_name="teachhelp";
+// Abre uma conexao com o BD.
 
-//pdo - somente orientado objeto
-$connect =mysqli_connect($servername,$username,$password,$db_name);
+$host        = "host = db4free.net;";
+$port        = "port = 3306;";
+$dbname      = "dbname = teachhelp;";
+$dbuser 	 = "teachhelp";
+$dbpassword	 = "d4pnw-d3@*ZRNkF";
 
-//codifica com o caracteres ao manipular dados do banco de dados
-//mysqli_set_charset($connect, "utf8");
 
-if(mysqli_connect_error()):
-	echo "Falha na conexão: ". mysqli_connect_error();
-endif;
+
+// dados de conexao com o b4app. Usar somente caso esteja usando b4app
+//$host        = "host = " . getenv("BD_HOST") . ";";
+//$port        = "port = " . getenv("BD_PORT") . ";";
+//$dbname      = "dbname = " . getenv("BD_DATABASE") . ";";
+//$dbuser 	 = getenv("BD_USER");
+//$dbpassword	 = getenv("BD_PASSWORD");
+
+// para conectar ao mysql, substitua pgsql por mysql
+$db_con= new PDO('mysql:' . $host . $port . $dbname, $dbuser, $dbpassword);
+
+//alguns atributos de performance.
+$db_con->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+$db_con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 ?>
