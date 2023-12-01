5e44c0e3671814db1517acd83145218fee90e945
davi1803 <92159621+Davi1803@users.noreply.github.com>
2023-12-01 15:14:06 -0300

    Update cadastrar.php

--- mobile/cadastrar.php
+++ mobile/cadastrar.php
@@ -15,7 +15,7 @@ if (isset($_POST['novo_nome']) && isset($_POST['novo_email']) && isset($_POST['n
  
     // o método trim elimina caracteres especiais/ocultos da string
 	$novo_nome = trim($_POST['novo_nome']);
-	$nova_email = trim($_POST['novo_email']);
+	$novo_email = trim($_POST['novo_email']);
   	$novo_senha = trim($_POST['nova_senha']);
   	$nova_data_nasc = trim($_POST['nova_data_nasc']);
   	$nova_data_nasc2 = str_replace("/", "-", $nova_data_nasc);
